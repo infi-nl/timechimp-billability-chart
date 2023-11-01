d69644c0d164814601cbeb1f3ce5450dd0b4233a
Luca Scalzotto <luca@scorpiac.com>
2023-11-01 21:10:16 +0100

    Fix week numbers according to Dutch system

--- src/date.ts
+++ src/date.ts
@@ -1,7 +1,13 @@
 import getWeekDateFns from 'date-fns/getWeek';
 
 export function getWeek(date: string | Date) {
-    return getWeekDateFns(new Date(date));
+    // See: https://date-fns.org/v2.30.0/docs/getWeek
+    return getWeekDateFns(new Date(date), {
+        // Weeks start on a Monday.
+        weekStartsOn: 1,
+        // Week number 1 should contain the 4th on January.
+        firstWeekContainsDate: 4,
+    });
 }
 
 export function toIsoDate(date: number | Date) {
